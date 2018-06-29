c78cdc556d0d5fa97eb4fa77b6e2884348876df8
Reka-Csima <rcsima7@gmail.com>
2018-06-29 16:40:42 +0200

    Set speed on the X axis to equal delta time

--- js/app.js
+++ js/app.js
@@ -17,6 +17,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    //Set speed on the X axis to equal delta time
+    this.x = this.speed * dt
 };
 
 // Draw the enemy on the screen, required method for game
